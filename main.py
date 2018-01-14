7f6d8024528e4373306cc7ea77b991c7e4a08c49
Will Wen <will.wen1@gmail.com>
2018-01-14 02:29:40 +0000

    add libraries and i2c with flask

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+#For Python 2.7
 import smbus
 import time
+
+#flask web server modules
+from flask import Flask
+from flask import request
+
 # for RPI version 1, use "bus = smbus.SMBus(0)"
 bus = smbus.SMBus(1)
 
 # This is the address we setup in the Arduino Program
 address = 0x04
 
+#put function declerations first
 def writeNumber(value):
     bus.write_byte(address, value)
     # bus.write_byte_data(address, 0, value)
@@ -16,17 +23,32 @@ def readNumber():
     # number = bus.read_byte_data(address, 1)
     return number
 
-while True:
-    var = input("Enter 1 - 9 ")
-    if not var:
-        continue
+app = Flask(__name__)
+@app.route('/servo')
+def index():
+    queryParam = "blinds"
+    setState = request.args.get(queryParam)
+    if(setState == "on"):
+        writeNumber(1); #1 denotes to turn on
+        print "RPI: Hi Arduino, I sent you 1"
+        # sleep one second
+        time.sleep(1)
 
-    writeNumber(var)
-    print "RPI: Hi Arduino, I sent you ", var
-    # sleep one second
-    time.sleep(1)
+        number = readNumber()
+        print "Arduino: Hey RPI, I received a digit ", number
+        return "Going to turn the blinds ON"
+    elif (setState == "off"):
+        writeNumber(2);
+        print "RPI: Hi Arduino, I sent you 2 , Meaning OFF"
+        # sleep one second
+        time.sleep(1)
 
-    number = readNumber()
-    print "Arduino: Hey RPI, I received a digit ", number
+        number = readNumber()
+        print "Arduino: Hey RPI, I received a digit ", number
+        return "Going to turn the blinds OFF"
+    else:
+        return "<h2>"+"Please set the query parameter: " + queryParam + "</h2>"
+if __name__ == '__main__':
+    app.run(debug=True)
 
-    print
+
